dd0322508b55f6fb90410ac5be693316c6a5c071
nesrinatktrk <79600556+nesrinatktrk@users.noreply.github.com>
2021-03-17 15:40:58 +0300

    Update print.py

--- print.py
+++ print.py
@@ -1,3 +1,3 @@
-print ("Merhaba")
-print ("""Yazılım
-başladı""")
+print ("Merhaba") #normal yazı çıktısı almak için tek tırnak yeterli
+print ("""Yazılım 
+başladı""") #ayrı ayrı print yazarak kullanmaktansa (""") kulllan
