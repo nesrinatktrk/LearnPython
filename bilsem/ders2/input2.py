198b669854b340dc3aefb0d28b90d75c794c4301
nesrinatktrk <79600556+nesrinatktrk@users.noreply.github.com>
2021-04-29 15:20:41 +0300

    Add files via upload

---
+++ bilsem/ders2/input2.py
@@ -0,0 +1,3 @@
+sayi1=input("1. sayiyi girin:") #input değeri stringtir. İşlem yapacaksanint'e dönüştürmelisin.
+sayi2=input("2. sayiyi girin:")
+print (int(sayi1)+ int(sayi2))
---
+++ bilsem/ders2/input3.py
@@ -0,0 +1,3 @@
+sayi1=int(input("Sayı giriniz..."))
+sayi2=int(input("2. sayıyı giriniz..."))
+print(sayi1 + sayi2)
