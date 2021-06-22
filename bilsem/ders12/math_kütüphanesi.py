71bb09ed56979647395d74f544689926d06f0784
nesrinatktrk <79600556+nesrinatktrk@users.noreply.github.com>
2021-06-22 14:02:24 +0300

    Add files via upload

---
+++ bilsem/ders12/math_kütüphanesi.py
@@ -0,0 +1,9 @@
+import math
+print(math.factorial(5)) #fonksiyon alır
+print(math.ceil(25.03)) #bir üste yuvarlar
+print(math.floor(25.99)) #bir alta yuvarlar
+print(math.gcd(6,3,9)) #ebob verir
+print(math.pow(2,5)) #2'nin 5.kuvvetini hesaplar
+print(math.sqrt(25)) #karekökünü alır
+print(math.fabs(-25)) #mutlak değer alır
+print(math.pi)
---
+++ bilsem/ders12/random_kütüphanesi.py
@@ -0,0 +1,5 @@
+import random
+#print(dir(random)) modülün içindekileri görmek için dir fonksiyonu kullanılır
+#print(help(random)) modül hakkında bilgi verir
+tuttuğum_sayi=random.randint(1,90)
+print(tuttuğum_sayi)
